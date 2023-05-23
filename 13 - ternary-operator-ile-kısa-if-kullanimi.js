5a184498e5942b426e5a5f44d99a71874e2e757c
Mete Tatar <mete-tatar@hotmail.com>
2023-05-23 12:32:13 +0300

    Update

---
+++ 13 - ternary-operator-ile-kısa-if-kullanimi.js
@@ -0,0 +1,9 @@
+// eğer kullanıcı adı varsa yazdır yoksa kullanıcı bilgisi bulunamadı yazdır
+
+let username = prompt("Kullanıcı adınızı giriniz = ")
+let info = document.querySelector("#info")
+
+// ternary kullanımı
+// koşul ? doğruysa : yanlışsa
+
+info.innerHTML = `${username.length > 0 ? username : "kullanıcı bilgisi bulunamadı"}`
