e71012debb873ee66cc415c8465fc5fc7aede2ed
Mete Tatar <mete-tatar@hotmail.com>
2023-05-27 21:46:05 +0300

    Update

---
+++ 15 - fonksiyonlar.js
@@ -0,0 +1,15 @@
+// İlk fonsiyonumuzu tanımlamak
+
+function name ()
+{
+    console.log("Mete")
+}
+
+function hello ()
+{
+    console.log("Merhaba")
+    name()
+}
+
+hello() // fonksiyonu çalıştırma komutu
+
