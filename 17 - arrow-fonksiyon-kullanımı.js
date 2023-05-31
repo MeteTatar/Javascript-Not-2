60e51b8526446a7491d269a4245e0541ee12f88d
Mete Tatar <mete-tatar@hotmail.com>
2023-05-31 11:48:49 +0300

    Update

---
+++ 17 - arrow-fonksiyon-kullanımı.js
@@ -0,0 +1,8 @@
+// Arrow function kullanımı
+
+function hello(firstname) // hello fonksiyonumuza firstname değişkeni tanımladık
+{
+    console.log(`Merhaba ${firstname}`) // consola merhaba yazdırıp firstname değişkenini yazdırıyoruz
+}
+
+hello("Mete") // firstname değişkenimize "Mete" string ini tanımlıyoruz
