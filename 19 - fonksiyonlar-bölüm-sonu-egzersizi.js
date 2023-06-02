a9b34f16a3509436fa2fe5e2884e1d8d20ff2c91
Mete Tatar <mete-tatar@hotmail.com>
2023-06-02 15:22:17 +0300

    Update

--- 19 - fonksiyonlar-bölüm-sonu-egzersizi.js
+++ 19 - fonksiyonlar-bölüm-sonu-egzersizi.js
@@ -1,19 +1,21 @@
 // fonksiyonlar bölüm sonu egzersizi
 
-let counter = 0
-let counterDOM = document.querySelector('#counter')
-let increaseDOM = document.querySelector('#increase')
-let decreaseDOM = document.querySelector('#decrease')
+let counter = 0 // counter adında bir değişken tanımlanır ve 0 değeri atanır
+let counterDOM = document.querySelector('#counter') // counterDOM adında bir değişken tanımlanır ve DOM'daki #counter elementi ile eşleştirilir
+let increaseDOM = document.querySelector('#increase') // increaseDOM adında bir değişken tanımlanır ve DOM'daki #increase elementi ile eşleştirilir
+let decreaseDOM = document.querySelector('#decrease') // decreaseDOM adında bir değişken tanımlanır ve DOM'daki #decrease elementi ile eşleştirilir
 
-counterDOM.innerHTML = counter
 
-increaseDOM.addEventListener("click" , clickevent)
-decreaseDOM.addEventListener("click" , clickevent)
+counterDOM.innerHTML = counter // counterDOM'un HTML içeriği counter değişkeninin mevcut değeriyle değiştirilir
 
-function clickevent()
+increaseDOM.addEventListener("click" , clickevent) // increaseDOM'a bir click olayı dinleyicisi eklenir ve clickevent fonksiyonu çağrılır
+decreaseDOM.addEventListener("click" , clickevent) // decreaseDOM'a bir click olayı dinleyicisi eklenir ve clickevent fonksiyonu çağrılır
+
+
+function clickevent() // clickevent adında bir fonksiyon tanımlanır, bu fonksiyon sayesinde sayaç arttırılıp azaltılacaktır
 {
-    console.log(this.id)
+    console.log(this.id) // console.log fonksiyonu aracılığıyla tıklanan butonun id'si konsola yazdırılır
 
-    this.id == "increase" ? counter += 1 : counter -= 1
-    counterDOM.innerHTML = counter
+    this.id == "increase" ? counter += 1 : counter -= 1 // Ternary operatörü kullanarak, tıklanan butonun id'sine göre sayaç değeri arttırılır veya azaltılır
+    counterDOM.innerHTML = counter// counterDOM'un HTML içeriği güncellenir ve sayaç değeri görüntülenir
 }
