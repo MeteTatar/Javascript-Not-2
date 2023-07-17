19bd2452e87e68aedac81ae9d6e3515d1ef62318
Mete Tatar <mete-tatar@hotmail.com>
2023-07-17 17:56:54 +0300

    Update

---
+++ 27 - for-döngüsü-kullanım.js
@@ -0,0 +1,14 @@
+// For döngüsü kullanımı
+
+
+// for(başlangıç değeri; koşul; artış miktarı) 
+
+// Başlangıç değeri: Döngünün başlangıç değerini belirtir.
+// Koşul: Döngünün çalışması için sağlanması gereken koşulu belirtir.
+// Artış miktarı: Döngü her çalıştığında başlangıç değeri artış miktarı kadar artar.
+
+for (let i = 1; i <= 10; i++)
+{
+    document.write(i);
+}
+  
